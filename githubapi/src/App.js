16cbb75911612bedd9104946470342f7305c2016
givaldo santos <givaldosantos@live.com>
2021-07-14 15:42:35 -0300

    home em andamento
    integrada com a API

--- githubapi/src/App.js
+++ githubapi/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+import './App.css'
 
 class App extends React.Component{
   state = {
@@ -19,11 +20,14 @@ class App extends React.Component{
   render(){
     return(
       <div>
-        <strong>Olá, {this.props.profile} </strong>
+        <p>GitSearch </p>
+        <strong>Informe o nome do usuário e verifique 
+          <br/>quantas contribuições foram realizadas 
+          <br/>no GitHub nos últimos anos. {this.props.profile} </strong>
         <div>
-          Digite User: <input type="text" onChange={this.handleTextChange.bind(this)}/>
+          <input type="text" placeholder="Digite aqui o usuário do GitHub" onChange={this.handleTextChange.bind(this)}/>
           {Object.entries(this.state.profile).map((values, key)=>(
-            <h2 key={key}><strong>{values[0]}:</strong>{values[1]}</h2>
+            <h4 key={key}><strong>{values[0]}:</strong>{values[1]}</h4>
           ))}
         </div>
       </div>
